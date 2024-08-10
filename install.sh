31301094244e3ab2be15735e9805bff899a98a5e
Diogo Soares <info@diogosoares.ca>
2024-08-10 14:26:16 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
+read -p "Node name: " node_name
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
+sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget screen
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
@@ -15,14 +18,17 @@ echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
 
 wget ${GITHUB_DOWNLOAD_URL} -O ~/ccminer/ccminer
 wget http://10.13.13.124/config_verus.json -O ~/ccminer/config.json
+echo $node_name > ~/nodeName.txt
 sed -i "s/Donator##/$(cat ~/nodeName.txt)/g" ~/ccminer/config.json
 chmod +x ~/ccminer/ccminer
 
 cat << EOF > ~/ccminer/start.sh
 #!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config.json
+screen -S miner ~/ccminer/ccminer -c ~/ccminer/config.json
 EOF
+
 chmod +x start.sh
+
 
 echo "setup nearly complete."
 echo "Edit the config with \"nano ~/ccminer/config.json\""
